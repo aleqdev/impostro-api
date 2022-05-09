104d2d76a4d63fef8887e48f4de2e19012d5377c
necromfox <necromfox@gmail.com>
2022-05-09 19:55:50 +0300

    env port

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::sync::Mutex;
-
+use std::env;
 use actix_web::{
     web::{self, Data},
     App, HttpServer,
@@ -11,9 +11,15 @@ pub type DataTy = Data<Mutex<impostro_shared::ImpostroData>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     let data = Data::new(Mutex::new(
         impostro_shared::ImpostroData::default(),
     ));
+
     HttpServer::new(move || {
         App::new()
             .app_data(data.clone())
@@ -23,7 +29,7 @@ async fn main() -> std::io::Result<()> {
             .route("validate_session_id", web::post().to(api::validate_session_id_fn))
             .route("create_session", web::post().to(api::create_session_fn))
     })
-    .bind(("127.0.0.1", 9001))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
